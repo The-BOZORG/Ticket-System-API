67112db6150aff8a1d00185abde3bfa908544909
soroush <soroushfalaki24@hotmail.com>
2026-08-05 17:33:02 +0330

    setup db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import envValidation from './config/env.validate';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeOrm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/typeOrm.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export async function typeOrmConfig(
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> {
+  return {
+    type: 'postgres',
+    host: configService.getOrThrow<string>('DATABASE_HOST'),
+    port: Number(configService.getOrThrow<string>('DATABASE_PORT')),
+    username: configService.getOrThrow<string>('DATABASE_USER'),
+    password: configService.getOrThrow<string>('DATABASE_PASSWORD'),
+    database: configService.getOrThrow<string>('DATABASE_NAME'),
+  };
+}
