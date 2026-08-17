5ba5fa4b986163ac502bb517377490adb3358ca5
soroush <soroushfalaki24@hotmail.com>
2026-08-17 13:33:29 +0330

    add user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { CurrentUser } from 'src/auth/decorators/currentUser.decorator';
+import { UserEntity } from './entities/user.entity';
+import { UpdateDto } from './dto/update.dto';
+import { ChangePasswordDto } from './dto/updatePassword.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Patch('update')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  public update(@Body() dto: UpdateDto, @CurrentUser() user: UserEntity) {
+    return this.usersService.update(dto, user.id);
+  }
+
+  @Patch('updatePass')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  public updatePass(
+    @Body() dto: ChangePasswordDto,
+    @CurrentUser() user: UserEntity,
+  ) {
+    return this.usersService.updatePass(dto, user.id);
+  }
+
+  @Delete('delete')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  public delete(@CurrentUser() user: UserEntity) {
+    return this.usersService.delete(user.id);
+  }
 }
