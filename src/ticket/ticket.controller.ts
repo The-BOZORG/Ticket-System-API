ae5f1a1d72064c0871df5a8fbb8f74ff1b8e61b1
soroush <soroushfalaki24@hotmail.com>
2026-08-18 20:53:52 +0330

    add ticket controllers

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,7 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { TicketService } from './ticket.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { CreateDto } from './dto/create.dto';
+import { CurrentUser } from 'src/auth/decorators/currentUser.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { Paginate } from 'nestjs-paginate';
+import type { PaginateQuery } from 'nestjs-paginate';
 
 @Controller('ticket')
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
+
+  @Post('create')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  public createTicket(@Body() dto: CreateDto, @CurrentUser() user: UserEntity) {
+    return this.ticketService.create(dto, user.id);
+  }
+
+  @Get('all')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  public getAllTickets(@Paginate() query: PaginateQuery) {
+    return this.ticketService.getAll(query);
+  }
+
+  @Get('my/:id')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  public async myTicket(
+    @Param('id') ticketId: number,
+    @CurrentUser() user: UserEntity,
+  ) {
+    return this.ticketService.getMyTicket(ticketId, user.id);
+  }
+
+  @Delete('my/:id')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  public async deleteTicket(
+    @Param('id') ticketId: number,
+    @CurrentUser() user: UserEntity,
+  ) {
+    return this.ticketService.delete(ticketId, user.id);
+  }
 }
