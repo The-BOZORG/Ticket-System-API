824fc7da6212dcb93d5f25f2f691fdecbea864f2
soroush <soroushfalaki24@hotmail.com>
2026-08-18 20:42:59 +0330

    add ticket service

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -4,10 +4,20 @@ import { TicketController } from './ticket.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketEntity } from './entities/ticket.entity';
 import { UsersModule } from 'src/users/users.module';
+import { CreateTicketProvider } from './providers/createTicket.provider';
+import { DeleteTicketProvider } from './providers/deleteTicket';
+import { GetAllTicketsPagination } from './providers/getAllTickets.provider';
+import { GetTicketProvider } from './providers/getTicketById';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forFeature([TicketEntity])],
   controllers: [TicketController],
-  providers: [TicketService],
+  providers: [
+    TicketService,
+    CreateTicketProvider,
+    DeleteTicketProvider,
+    GetAllTicketsPagination,
+    GetTicketProvider,
+  ],
 })
 export class TicketModule {}
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTicketProvider } from './providers/createTicket.provider';
+import { GetAllTicketsPagination } from './providers/getAllTickets.provider';
+import { GetTicketProvider } from './providers/getTicketById';
+import { DeleteTicketProvider } from './providers/deleteTicket';
+import { CreateDto } from './dto/create.dto';
+import { TicketEntity } from './entities/ticket.entity';
+import { Paginated, PaginateQuery } from 'nestjs-paginate';
 
 @Injectable()
-export class TicketService {}
+export class TicketService {
+  constructor(
+    private readonly createTicket: CreateTicketProvider,
+    private readonly getAllTicket: GetAllTicketsPagination,
+    private readonly getTicket: GetTicketProvider,
+    private readonly deleteTicket: DeleteTicketProvider,
+  ) {}
+
+  public create(dto: CreateDto, userId: string): Promise<TicketEntity> {
+    return this.createTicket.create(dto, userId);
+  }
+
+  public getAll(query: PaginateQuery): Promise<Paginated<TicketEntity>> {
+    return this.getAllTicket.getAllTicketsPagination(query);
+  }
+
+  public getMyTicket(ticketId: number, userId: string): Promise<TicketEntity> {
+    return this.getTicket.getTicket(ticketId, userId);
+  }
+
+  public delete(ticketId: number, userId: string): Promise<void> {
+    return this.deleteTicket.deleteTicket(ticketId, userId);
+  }
+}
