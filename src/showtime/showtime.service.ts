479c2a4b84d136d8d9735c7d8b6293616caaa859
soroush <soroushfalaki24@hotmail.com>
2026-08-20 12:05:57 +0330

    add showtime services

--- src/showtime/showtime.service.ts
+++ src/showtime/showtime.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { CreateShowtimeProvider } from './providers/createShowtime.provider';
+import { FindShowtimeByIdProvider } from './providers/findShowtimeById.provider';
+import { UpdateShowtimeProvider } from './providers/updateShowtime.provider';
+import { DeleteShowtimeProvider } from './providers/deleteShowTime.provider';
 import { CreateShowtimeDto } from './dto/createShowtime.dto';
+import { ShowtimeEntity } from './entities/showtime.entity';
 import { UpdateShowtimeDto } from './dto/update-showtime.dto';
 
 @Injectable()
 export class ShowtimeService {
-  create(createShowtimeDto: CreateShowtimeDto) {
-    return 'This action adds a new showtime';
+  constructor(
+    private readonly createShowtimeProvider: CreateShowtimeProvider,
+    private readonly findShowtimeByIdProvider: FindShowtimeByIdProvider,
+    private readonly updateShowtimeProvider: UpdateShowtimeProvider,
+    private readonly deleteShowtimeProvider: DeleteShowtimeProvider,
+  ) {}
+
+  public async create(dto: CreateShowtimeDto): Promise<ShowtimeEntity> {
+    return this.createShowtimeProvider.create(dto);
   }
 
-  findAll() {
-    return `This action returns all showtime`;
+  public async findById(id: number): Promise<ShowtimeEntity> {
+    return this.findShowtimeByIdProvider.findById(id);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} showtime`;
+  public async update(
+    id: number,
+    dto: UpdateShowtimeDto,
+  ): Promise<ShowtimeEntity> {
+    return this.updateShowtimeProvider.update(id, dto);
   }
 
-  update(id: number, updateShowtimeDto: UpdateShowtimeDto) {
-    return `This action updates a #${id} showtime`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} showtime`;
+  public async delete(id: number): Promise<void> {
+    return this.deleteShowtimeProvider.delete(id);
   }
 }
