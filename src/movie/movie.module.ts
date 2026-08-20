c8e80a846dc8fda141da1da264e33bbc43362b82
soroush <soroushfalaki24@hotmail.com>
2026-08-20 11:33:50 +0330

    complete movie module

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieEntity } from './entities/movie.entites';
+import { CreateMovieProvider } from './providers/createMovie.provider';
+import { FindMovieByIdProvider } from './providers/findMoveById.provider';
+import { GetAllMoviesPagination } from './providers/getAllMove.provider';
+import { UpdateMovieProvider } from './providers/updateMovie.provider';
+import { DeleteMovieProvider } from './providers/deleteMovie';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MovieEntity])],
   controllers: [MovieController],
-  providers: [MovieService],
+  providers: [
+    MovieService,
+    CreateMovieProvider,
+    FindMovieByIdProvider,
+    GetAllMoviesPagination,
+    UpdateMovieProvider,
+    DeleteMovieProvider,
+  ],
 })
 export class MovieModule {}
