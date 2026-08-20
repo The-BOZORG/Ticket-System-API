c1368e5e866bb58e6fb8badbf9a1ce007dad6b65
soroush <soroushfalaki24@hotmail.com>
2026-08-20 12:10:00 +0330

    add showtime controllers

--- src/showtime/showtime.controller.ts
+++ src/showtime/showtime.controller.ts
@@ -1,45 +1,53 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
 } from '@nestjs/common';
+
 import { ShowtimeService } from './showtime.service';
+import { ShowtimeEntity } from './entities/showtime.entity';
 import { CreateShowtimeDto } from './dto/createShowtime.dto';
 import { UpdateShowtimeDto } from './dto/update-showtime.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
-@Controller('showtime')
+@Controller('showtimes')
 export class ShowtimeController {
   constructor(private readonly showtimeService: ShowtimeService) {}
 
   @Post()
-  create(@Body() createShowtimeDto: CreateShowtimeDto) {
-    return this.showtimeService.create(createShowtimeDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.showtimeService.findAll();
+  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(JwtAuthGuard)
+  public async create(@Body() dto: CreateShowtimeDto): Promise<ShowtimeEntity> {
+    return this.showtimeService.create(dto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.showtimeService.findOne(+id);
+  @HttpCode(HttpStatus.OK)
+  public async findById(@Param('id') id: number): Promise<ShowtimeEntity> {
+    return this.showtimeService.findById(id);
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateShowtimeDto: UpdateShowtimeDto,
-  ) {
-    return this.showtimeService.update(+id, updateShowtimeDto);
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  public async update(
+    @Param('id') id: number,
+    @Body() dto: UpdateShowtimeDto,
+  ): Promise<ShowtimeEntity> {
+    return this.showtimeService.update(id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.showtimeService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  public async delete(@Param('id') id: number): Promise<void> {
+    return this.showtimeService.delete(id);
   }
 }
--- src/showtime/showtime.module.ts
+++ src/showtime/showtime.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ShowtimeService } from './showtime.service';
 import { ShowtimeController } from './showtime.controller';
+import { CreateShowtimeProvider } from './providers/createShowtime.provider';
+import { FindShowtimeByIdProvider } from './providers/findShowtimeById.provider';
+import { UpdateShowtimeProvider } from './providers/updateShowtime.provider';
+import { DeleteShowtimeProvider } from './providers/deleteShowTime.provider';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShowtimeEntity } from './entities/showtime.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ShowtimeEntity])],
   controllers: [ShowtimeController],
-  providers: [ShowtimeService],
+  providers: [
+    ShowtimeService,
+    CreateShowtimeProvider,
+    FindShowtimeByIdProvider,
+    UpdateShowtimeProvider,
+    DeleteShowtimeProvider,
+  ],
 })
 export class ShowtimeModule {}
